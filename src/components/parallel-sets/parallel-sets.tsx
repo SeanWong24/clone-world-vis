6149400b40a4731bda4c9fd9f844dca8cb45f15c
Sean Wong <seanwong24@live.com>
2020-02-11 11:17:48 +0000

    re-implement renderRibbons() for parallel-sets

--- src/components/parallel-sets/parallel-sets.tsx
+++ src/components/parallel-sets/parallel-sets.tsx
@@ -37,106 +37,157 @@ export class ParallelSets {
     const dimemsionNameList = (this.dimensions?.length > 0) ? this.dimensions : Object.keys(this.data[0]);
     const rootNode = new DataNode(undefined, undefined, undefined, this.data).initialize(dimemsionNameList);
 
-    const depthNodeListMap = new Map<number, DataNode[]>();
+    const depthSegmentMap = new Map<number, Map<string, DataNode[]>>();
     rootNode.dfs(node => {
-      const nodeList = depthNodeListMap.get(node.depth);
+      let segmentNodeListMap = depthSegmentMap.get(node.depth);
+      if (!segmentNodeListMap) {
+        segmentNodeListMap = new Map();
+        depthSegmentMap.set(node.depth, segmentNodeListMap);
+      }
 
+      const nodeList = segmentNodeListMap.get(node.valueName);
       if (nodeList) {
-        nodeList.push(node);
+        let indexToInsert = nodeList.findIndex(n => node.compare(n) < 1);
+        if (indexToInsert < 0) {
+          indexToInsert = nodeList.length;
+        }
+        nodeList.splice(indexToInsert, 0, node);
       } else {
-        depthNodeListMap.set(node.depth, [node]);
+        segmentNodeListMap.set(node.valueName, [node]);
       }
     });
-
-    depthNodeListMap.forEach(value => value.sort((a, b) => a.compare(b)));
 
     return (
       <Host>
         <svg ref={el => this.mainSvgElement = el} id="main-svg" width="100%" height="100%">
           <g id="textures"></g>
           <g id="axes"></g>
-          {this.mainSvgElementDimensions && this.renderRibbons(depthNodeListMap, dimemsionNameList)}
+          {this.mainSvgElementDimensions && this.renderRibbons(depthSegmentMap, dimemsionNameList)}
         </svg>
       </Host>
     );
   }
 
-  private renderRibbons(depthNodeListMap: Map<number, DataNode[]>, dimensionNameList: string[]) {
-    const currentLayerRibbonGElements = [...depthNodeListMap].map(([depth, nodeList]) => {
-      if (depth > 1) {
-        const currentLayerRibbonPathes = nodeList.map((currentNode, currentNodeIndex) => {
-          const d = this.obatinD(depthNodeListMap, dimensionNameList, depth, currentNode, currentNodeIndex);
+  private renderRibbons(depthSegmentMap: Map<number, Map<string, DataNode[]>>, dimensionNameList: string[]) {
+    const segmentMargin = 20;
 
-          const onMouseEnterCallback = () => {
-            const nodeHierarchyList = [currentNode];
-            let walker = currentNode;
-            while (walker.parentNode) {
-              walker = walker.parentNode;
-              nodeHierarchyList.unshift(walker);
-            }
-            d3.select(this.mainSvgElement).select('#ribbons').selectAll('path')
-              .filter(n => (nodeHierarchyList.find(d => d === n) as unknown as boolean))
-              .attr('opacity', .9);
-          };
-          const onMouseOutCallback = () => d3.select(this.mainSvgElement).select('#ribbons').selectAll('path').attr('opacity', .5);
+    const currentLayerRiboonsG = [...depthSegmentMap].map(([currentDepth, segmentNodeListMap]) => {
+      if (currentDepth > 1) {
+        const currentSegmentRibbonsG = [...segmentNodeListMap].map(([, nodeList], currentSegmentIndex) => {
+          const currentSegmentRibbonPathes = nodeList.map((currentNode, currentNodeIndex) => {
+            const d = this.obatinD(segmentMargin, depthSegmentMap, segmentNodeListMap, currentSegmentIndex, currentDepth, currentNode, currentNodeIndex, dimensionNameList);
 
-          const path = <path
-            ref={el => d3.select(el).datum(currentNode)}
-            d={d}
-            stroke="black"
-            stroke-width="1"
-            fill={this.ribbonFillCallback(currentNode, d3.select(this.mainSvgElement).select('#textures'))}
-            opacity=".5"
-            cursor="pointer"
-            onMouseEnter={onMouseEnterCallback}
-            onMouseOut={onMouseOutCallback}
-            onClick={() => this.ribbonClick.emit(currentNode)}>
-            <title>{currentNode.parentNode?.valueName + '=>' + currentNode.valueName + ',' + currentNode.dataRecordCount}</title>
-          </path>;
-          return path;
-        })
-        return <g id={'ribbon-layer-' + (depth - 1) + '-' + depth} class="ribbon-layer">{currentLayerRibbonPathes}</g>;
+            const onMouseEnterCallback = () => {
+              const nodeHierarchyList = [currentNode];
+              let walker = currentNode;
+              while (walker.parentNode) {
+                walker = walker.parentNode;
+                nodeHierarchyList.unshift(walker);
+              }
+              d3.select(this.mainSvgElement).select('#ribbons').selectAll('path')
+                .filter(n => (nodeHierarchyList.find(d => d === n) as unknown as boolean))
+                .attr('opacity', .9);
+            };
+            const onMouseOutCallback = () => d3.select(this.mainSvgElement).select('#ribbons').selectAll('path').attr('opacity', .5);
+
+            const path = <path
+              ref={el => d3.select(el).datum(currentNode)}
+              d={d}
+              stroke="black"
+              stroke-width="1"
+              fill={this.ribbonFillCallback(currentNode, d3.select(this.mainSvgElement).select('#textures'))}
+              opacity=".5"
+              cursor="pointer"
+              onMouseEnter={onMouseEnterCallback}
+              onMouseOut={onMouseOutCallback}
+              onClick={() => this.ribbonClick.emit(currentNode)}>
+              <title>{currentNode.parentNode?.valueName + '=>' + currentNode.valueName + ',' + currentNode.dataRecordCount}</title>
+            </path>;
+            return path;
+          });
+          return <g id={'ribbon-layer-' + (currentDepth - 1) + '-to-' + currentDepth + '-segment-' + currentSegmentIndex} class="ribbon-layer-segment">{currentSegmentRibbonPathes}</g>;
+        });
+        return <g id={'ribbon-layer-' + (currentDepth - 1) + '-to-' + currentDepth} class="ribbon-layer">{currentSegmentRibbonsG}</g>;
       }
     });
 
-    return <g id="ribbons">{currentLayerRibbonGElements}</g>;
+    return <g id="ribbons">{currentLayerRiboonsG}</g>;
   }
 
-  private obatinD(depthNodeListMap: Map<number, DataNode[]>, dimensionNameList: string[], depth: number, currentNode: DataNode, currentNodeIndex: number) {
-    const positionScale = d3.scaleLinear()
+  private obatinD(segmentMargin: number, depthSegmentMap: Map<number, Map<string, DataNode[]>>, segmentNodeListMap: Map<string, DataNode[]>, currentSegmentIndex: number, currentDepth: number, currentNode: DataNode, currentNodeIndex: number, dimensionNameList: string[]) {
+    const parentNode = currentNode.parentNode;
+    let parentNodeSegmentIndex;
+    let parentNodeIndex;
+    const segmentNodeListMapForParentNode = [...depthSegmentMap.values()].find(
+      segmentMap => [...segmentMap.values()].find(
+        (nodeList, segmentIndex) => {
+          parentNodeIndex = nodeList.findIndex(node => node === parentNode);
+          const result = parentNodeIndex >= 0;
+          if (result) {
+            parentNodeSegmentIndex = segmentIndex;
+          }
+          return result;
+        }
+      )
+    );
+
+    const segmentCountForParentLayer = segmentNodeListMapForParentNode.size;
+    const positionScaleForParentLayer = d3.scaleLinear()
       .domain([0, this.data.length])
-      .range([10, this.mainSvgElementDimensions.height - 10]);
+      .range([segmentMargin, this.mainSvgElementDimensions.height - segmentCountForParentLayer * segmentMargin]);
 
-    const canvasWidth = this.mainSvgElement?.clientWidth;
+    const segmentCountForCurrentLayer = segmentNodeListMap.size;
+    const positionScaleForCurrentLayer = d3.scaleLinear()
+      .domain([0, this.data.length])
+      .range([segmentMargin, this.mainSvgElementDimensions.height - segmentCountForCurrentLayer * segmentMargin]);
+
+    const canvasWidth = this.mainSvgElementDimensions.width;
     const dimensionSplitCount = dimensionNameList.length - 1;
-    const parentDepth = depth - 1;
-    const parentIndex = (depthNodeListMap.get(parentDepth) || []).findIndex(d => d === currentNode.parentNode);
-    const currentIndexInParentChildMap = [...currentNode.parentNode?.childMap.entries() || []].findIndex(d => d[1] === currentNode);
+    const parentDepth = currentDepth - 1;
 
-    const previousLayerNodeList = depthNodeListMap.get(parentDepth);
-    const previousLayerNodeListBeforeParentNode = previousLayerNodeList?.slice(0, parentIndex);
-    const previousLayerNodeRecordCountBeforeParentNode = d3.sum(previousLayerNodeListBeforeParentNode?.map(node => node.dataRecordCount) || [0]);
+    const segmentLengthOffsetForParentNode = parentNodeSegmentIndex * segmentMargin;
+    const previousSegmentsRecordCountForParentNode = d3.sum(
+      [...segmentNodeListMapForParentNode.values()].slice(0, parentNodeSegmentIndex).map(
+        nodeList => d3.sum(
+          nodeList.map(
+            node => node.dataRecordCount
+          )
+        )
+      )
+    );
+    const currentSegmentRecordCountBeforeParentNode = d3.sum(
+      [...segmentNodeListMapForParentNode.values()][parentNodeSegmentIndex]
+        .slice(0, parentNodeIndex)
+        .map(node => node.dataRecordCount)
+    );
+    const parentNodeChildList = [...parentNode.childMap.values()];
+    const currentNodeIndexInParentChildList = parentNodeChildList.findIndex(node => node === currentNode);
+    const silingsBeforeCurrentNodeRecordCount = d3.sum(
+      parentNodeChildList.slice(0, currentNodeIndexInParentChildList).map(node => node.dataRecordCount)
+    );
 
-    const parentNodeChildMapEntryList = [...currentNode.parentNode?.childMap.entries() || []];
-    const siblingListBeforeCurrentNode = parentNodeChildMapEntryList.slice(0, currentIndexInParentChildMap);
-    const siblingRecordCountBeforeCurrentNode = d3.sum(siblingListBeforeCurrentNode.map(d => d[1].dataRecordCount));
-
-    const currentLayerNodeList = depthNodeListMap.get(depth);
-    const currentLayerNodeListBeforeCurrentNode = currentLayerNodeList?.slice(0, currentNodeIndex);
-    const cuurenLayerNodeRecordCountBeforeCurrentNode = d3.sum(currentLayerNodeListBeforeCurrentNode?.map(d => d.dataRecordCount) || [0]);
-
-    const currentLayerNodeListTillCurrentNode = currentLayerNodeList?.slice(0, currentNodeIndex + 1);
-    const cuurenLayerNodeRecordCountTillCurrentNode = d3.sum(currentLayerNodeListTillCurrentNode?.map(d => d.dataRecordCount) || [0]);
+    const segmentLengthOffsetForCurrentNode = currentSegmentIndex * segmentMargin;
+    const previousSegmentsRecordCountForCurrentNode = d3.sum(
+      [...segmentNodeListMap.values()].slice(0, currentSegmentIndex).map(
+        nodeList => d3.sum(
+          nodeList.map(
+            node => node.dataRecordCount
+          )
+        )
+      )
+    );
+    const currentSegmentRecordCountBeforeCurrentNode = d3.sum(
+      [...segmentNodeListMap.values()][currentSegmentIndex]
+        .slice(0, currentNodeIndex)
+        .map(node => node.dataRecordCount)
+    );
 
     const x1 = canvasWidth / dimensionSplitCount * (parentDepth - 1);
-    const y1 = positionScale(
-      previousLayerNodeRecordCountBeforeParentNode +
-      siblingRecordCountBeforeCurrentNode
-    );
-    const x2 = canvasWidth / dimensionSplitCount * (depth - 1);
-    const y2 = positionScale(cuurenLayerNodeRecordCountBeforeCurrentNode);
+    const y1 = positionScaleForParentLayer(previousSegmentsRecordCountForParentNode + currentSegmentRecordCountBeforeParentNode + silingsBeforeCurrentNodeRecordCount) + segmentLengthOffsetForParentNode;
+    const x2 = canvasWidth / dimensionSplitCount * (currentDepth - 1);
+    const y2 = positionScaleForCurrentLayer(previousSegmentsRecordCountForCurrentNode + currentSegmentRecordCountBeforeCurrentNode) + segmentLengthOffsetForCurrentNode;
     const x3 = x2;
-    const y3 = positionScale(cuurenLayerNodeRecordCountTillCurrentNode);
+    const y3 = positionScaleForCurrentLayer(previousSegmentsRecordCountForCurrentNode + currentSegmentRecordCountBeforeCurrentNode + currentNode.dataRecordCount) + segmentLengthOffsetForCurrentNode;
     const x4 = x1;
     const y4 = y1 + y3 - y2;
 
